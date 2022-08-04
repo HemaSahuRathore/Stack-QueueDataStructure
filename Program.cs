eea0f1aba17bdfa4fb6f73fcd8c1102b305a84ec
Hema <hemasahu019@gmail.com>
2022-08-05 00:22:17 +0530

    Added welcome page to main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StackNQueueDataStructure
+{ /// <summary>
+  /// Stack & Queue Data Structure
+  /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack & Queue Data Structure"); //display welcome msg
+        }
+    }
+}
