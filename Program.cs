7997bc470220788c047b9310b5690acc5990fdb5
Hema <hemasahu019@gmail.com>
2022-08-05 16:06:41 +0530

    Added UC2 code to peek and pop till stack become empty

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace StackNQueueDataStructure
 { /// <summary>
-  /// UC 1 : Ability to create a Stack of 56->30->70
+  ///UC 2 : Ability to peak and pop from the Stack till it is empty 56->30->70
   /// </summary>
     internal class Program
     {
@@ -14,7 +14,8 @@
 
                 Console.WriteLine("Please select the Options from Below: ");
                 Console.WriteLine("1.Ability to create a Stack of 56->30->70");
-                Console.Write("2.Exit  ");
+                Console.WriteLine("2.Ability to peak and pop from the Stack till it is empty 56->30->70");
+                Console.Write("3.Exit  ");
                 option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
@@ -25,10 +26,20 @@
                         stack.Push(30);
                         stack.Push(70);
                         stack.Display();
+                        Console.WriteLine();
                         break;
-                    
+                    case 2:
+                        Stack stackObj = new Stack();
+                        stackObj.Push(56);
+                        stackObj.Push(30);
+                        stackObj.Push(70);
+                        Console.WriteLine("Is stack Empty : " + stackObj.IsEmpty());
+                        Console.WriteLine();
+                        break;
+
+
                 }
-            } while (option != 2);
+            } while (option != 3);
 
 
         }
--- Stack.cs
+++ Stack.cs
@@ -11,6 +11,7 @@ namespace StackNQueueDataStructure
         public Node top;
         LinkedList linkedlistObj = new LinkedList();
 
+        //add data
         public void Push(int data)
         {
             Node newNode = new Node(data);
@@ -22,6 +23,7 @@ namespace StackNQueueDataStructure
             Console.WriteLine("{0} is added", data);
         }
         
+        //display element
         public void Display()
         {
             Node tempTop = top;
@@ -35,6 +37,47 @@ namespace StackNQueueDataStructure
             }
 
         }
+
+        //display top most data
+        public void peek() 
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty!! No elemt is present.");
+                return;
+            }               
+            else
+                Console.WriteLine("The top most element in stack is " + this.top.data);
+        }
+
+        //remove top most element
+        public void Pop()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is Empty!! No element is present");
+                return;
+            }
+            else
+            {
+                int deleteNode = top.data;
+                this.top = top.next; //assiging top to next element
+                Console.WriteLine("{0} is deleted from the top", deleteNode);
+            }
+        }
+
+        //return true if stack is empty else false
+        public bool IsEmpty()
+        {
+            while(this.top != null)
+            {
+                peek();
+                Pop();
+            }
+            return true;
+        }
+        
+       
 
     }
 }
